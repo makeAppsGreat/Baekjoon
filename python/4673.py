5104603da8939bcefd15b077bc930270ee344cd0
makeappsgreat <makeappsgreat@gmail.com>
2022-05-01 22:23:08 +0900

    2022-05-01

---
+++ python/4673.py
@@ -0,0 +1,23 @@
+
+
+if __name__ == "__main__":
+    l = [ i for i in range(1, 10000 + 1) ]
+
+
+    for i in range(1, 2 ** 32):
+        __sum = i
+
+        while True:
+            __sum += i % 10
+            i = i // 10
+            if i == 0: break
+        
+        try: l.remove(__sum)
+        except: pass
+        if __sum >= 20000: break
+
+
+    result = ""
+    for j in l: result += "{}\n".format(j)
+    print(result)
+
