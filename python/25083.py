5f41b7dabf937a53147031e0b3302d85b601bdec
makeappsgreat <makeappsgreat@gmail.com>
2022-05-05 20:33:17 +0900

    2022-05-05

---
+++ python/25083.py
@@ -0,0 +1,16 @@
+"""
+    25083번: 새싹
+    Create by Kim Gayoun on 2022-05-05
+"""
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    print("         ,r'\"7")
+    print("r`-_   ,'  ,/")
+    print(" \\. \". L_r'")
+    print("   `~\\/")
+    print("      |")
+    print("      |")
+
