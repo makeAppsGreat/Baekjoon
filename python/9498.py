f4581bbc808b1dfb5943a4915d56f389bfbefeff
makeappsgreat <makeappsgreat@gmail.com>
2022-06-07 18:50:29 +0900

    https://boj.kr/9498

---
+++ python/9498.py
@@ -0,0 +1,10 @@
+"""
+    9498번: 시험 성적
+    Create by Kim Gayoun on 2022-06-07
+"""
+S = int(input())
+if S >= 90: print("A")
+elif S >= 80: print("B")
+elif S >= 70: print("C")
+elif S >= 60: print("D")
+else: print("F")
